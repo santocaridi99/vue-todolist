c67f3f1760196011ef2afefa6e30206e484756a5
santocaridi99 <santocaridi99@gmail.com>
2022-01-17 15:41:17 +0100

    Prima Milestone

---
+++ js/script.js
@@ -0,0 +1,24 @@
+new Vue({
+    el:"#app",
+    data:{
+        tasks:[
+            {
+                text:'Fare i compiti',
+                done:true
+            },
+            {
+                text:'Fare la spesa',
+                done:false
+            },
+            {
+                text:'Fare il bucato',
+                done:true
+            }
+        ],
+        temporaryVariable:'',
+    },
+    methods:{
+
+    }
+
+})
