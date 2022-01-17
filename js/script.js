c908b242b0d35133d5df6a2db6ff96c7f830d455
santocaridi99 <santocaridi99@gmail.com>
2022-01-17 17:24:13 +0100

    aggiunto commenti

--- js/script.js
+++ js/script.js
@@ -15,15 +15,20 @@ new Vue({
                 done:true
             }
         ],
+        // creo un oggetto temporaneo con testo vuoto
         temporaryTask:{
             text:'',
             done:false
         },
     },
     methods:{
+        // removeTask fa lo splice nella posizione selezionata *index e una volta sola *1
         removeTask:function(index){
             this.tasks.splice(index,1);
         },
+        //add task effettua il push dell'oggetto temporaneo nell'array
+        //che viene modificato dal v-model nell'html
+        //dopo averlo pushato , si svuota
         addTask:function(){
             this.tasks.push(this.temporaryTask);
             this.temporaryTask={
@@ -31,6 +36,9 @@ new Vue({
                 done:false
             }
         },
+        // check dice che se nella posizione dell'indice, done è esempio true
+        // diventeà false e vicevera 
+        // servirà per aggiungere o togliere il done ad una classe
         check:function(index){
            this.tasks[index].done=!this.tasks[index].done
         }
